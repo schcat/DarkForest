8514bd78b49976cf8962f2703c6d275ab5a2896b
schcat <939851583@qq.com>
2015-04-28 05:51:17 +0800

    Create chooseWhite.java

---
+++ chooseWhite.java
@@ -0,0 +1,12 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class chooseWhite extends JPanel {
+		public void paintComponent(Graphics g){
+			g.fillOval(470, 15, 30, 30);
+			g.setColor(Color.white);
+			g.fillOval(472, 17, 26, 26);
+			}
+		}
