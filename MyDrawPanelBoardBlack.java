22d5d215d1f014d89f0e7c76797607307d7eb241
schcat <939851583@qq.com>
2015-04-28 05:55:03 +0800

    Create MyDrawPanelBoardBlack.java

---
+++ MyDrawPanelBoardBlack.java
@@ -0,0 +1,24 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+public class MyDrawPanelBoardBlack extends JPanel {
+	public void paintComponent(Graphics g){
+		for(int i = 1; i<10; i++){
+			for(int j = 1; j<10; j++){
+				if((Player.forestPlayerStatus[i][j]).equals("1")){
+					g.setColor(Color.black);
+					g.fillOval(133 + (j - 1) * 50, 42 + (i - 1) * 50, 36, 36);
+					}
+			}
+		}
+		for(int i = 1; i<10; i++){
+			for(int j = 1; j<10; j++){
+				if((Player.forestPlayerStatus[i][j]).equals("2")){
+					g.setColor(Color.white);
+					g.fillOval(133 + (j - 1) * 50, 42 + (i - 1) * 50, 36, 36);
+					}
+			}
+		}
+	}
+}
