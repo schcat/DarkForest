858a93ec3cb3ed0a9d1fafe8bdaae2a0ce0fa6b9
schcat <939851583@qq.com>
2015-04-28 05:50:37 +0800

    Create chooseBlack.java

---
+++ chooseBlack.java
@@ -0,0 +1,11 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class chooseBlack extends JPanel {
+		public void paintComponent(Graphics g){
+			g.setColor(Color.black);
+			g.fillOval(420, 15, 30, 30);
+			}
+		}
